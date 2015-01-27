c52913dbd347fc781d3fc92f4c7ce10628eda776
livroandroidup@gmail.com <livroandroidup@gmail.com>
2015-01-27 14:24:11 -0200

    OK

---
+++ Passo06-TabsCarros/Carros/app/src/main/res/layout/CarrosFragment.java
@@ -0,0 +1,18 @@
+package layout;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.livroandroid.carros.R;
+import br.com.livroandroid.carros.fragments.BaseFragment;
+
+public class CarrosFragment extends BaseFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_carros, container, false);
+        return view;
+    }
+}
