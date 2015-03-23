60e14f2687ebbfecfd31348442d649519f895438
Ricardo <livroandroid@gmail.com>
2015-03-23 10:02:18 -0300

    First commit

--- Passo20-Mapa/Carros/app/src/main/java/br/com/livroandroid/carros/fragments/MapaFragment.java
+++ Passo20-Mapa/Carros/app/src/main/java/br/com/livroandroid/carros/fragments/MapaFragment.java
@@ -1,18 +1,17 @@
 package br.com.livroandroid.carros.fragments;
 
 
-import android.net.Uri;
+import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.MediaController;
-import android.widget.VideoView;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -26,46 +25,69 @@ import br.com.livroandroid.carros.domain.Carro;
 /**
  * A simple {@link android.support.v4.app.Fragment} subclass.
  */
-public class MapaFragment extends BaseFragment implements OnMapReadyCallback {
+public class MapaFragment extends BaseFragment implements OnMapReadyCallback, GoogleMap.OnMyLocationChangeListener {
 
-
+    // Objeto que controla o Google Maps
     private GoogleMap map;
-    private Carro c;
+    private Carro carro;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_mapa, container, false);
 
+        // Recupera o fragment que está no layout
+        // Utiliza o getChildFragmentManager() pois é um fragment dentro do outro
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapFragment);
+
+        // Inicia o Google Maps dentro do fragment
         mapFragment.getMapAsync(this);
 
         setHasOptionsMenu(true);
 
-        this.c = (Carro) getArguments().getSerializable("carro");
+        this.carro = (Carro) getArguments().getSerializable("carro");
 
         return view;
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
+        // O método onMapReady(map) é chamado quando a inicialização do mapa estiver Ok.
         this.map = map;
 
-        if(c != null) {
-            Log.d(TAG, "Carro " + c.nome + " > " + c.urlFoto + ", lat/lng " + c.latitude + "/" + c.longitude);
+        if(carro != null) {
 
-            LatLng location = new LatLng(Double.parseDouble(c.latitude), Double.parseDouble(c.longitude));
+            // Ativa o botão para mostrar minha localização
+            map.setMyLocationEnabled(true);
 
-            //map.setMyLocationEnabled(true);
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 13));
+            // Cria o objeto LatLng com a coordenada da fábrica
+            LatLng location = new LatLng(Double.parseDouble(carro.latitude), Double.parseDouble(carro.longitude));
+            // Posiciona o mapa na coordenada da fábrica (zoom = 13)
 
+            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(location, 13);
+            //map.moveCamera(update);
+
+            map.animateCamera(update, 10000, new GoogleMap.CancelableCallback() {
+                @Override
+                public void onFinish() {
+                    Toast.makeText(getContext(), "Mapa centralizado.", Toast.LENGTH_SHORT).show();
+                }
+                @Override
+                public void onCancel() {
+                    Toast.makeText(getContext(), "Animação cancelada.", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            // Marcador no local da fábrica
             map.addMarker(new MarkerOptions()
-                    .title(c.nome)
-                    .snippet(c.desc)
+                    .title(carro.nome)
+                    .snippet(carro.desc)
                     .position(location));
 
-            // Other supported types include: MAP_TYPE_NORMAL,
+            map.setOnMyLocationChangeListener(this);
+
+            // Tipo do mapa: MAP_TYPE_NORMAL,
             // MAP_TYPE_TERRAIN, MAP_TYPE_HYBRID and MAP_TYPE_NONE
-            map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         }
     }
 
@@ -77,10 +99,10 @@ public class MapaFragment extends BaseFragment implements OnMapReadyCallback {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(map != null && c != null) {
+        if(map != null && carro != null) {
             if (item.getItemId() == R.id.action_location_carro) {
                 // Posiciona mapa na localização da fábrica
-                LatLng location = new LatLng(Double.parseDouble(c.latitude), Double.parseDouble(c.longitude));
+                LatLng location = new LatLng(Double.parseDouble(carro.latitude), Double.parseDouble(carro.longitude));
                 map.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 13));
             }else if (item.getItemId() == R.id.action_location_directions) {
                 // Posiciona mapa no usuário
@@ -95,8 +117,8 @@ public class MapaFragment extends BaseFragment implements OnMapReadyCallback {
             else if (item.getItemId() == R.id.action_mapa_normal) {
                 // Modo Normal
                 map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                // Modo Satélite
             } else if (item.getItemId() == R.id.action_mapa_satelite) {
+                // Modo Satélite
                 map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
             } else if (item.getItemId() == R.id.action_mapa_terreno) {
                 // Modo Terreno
@@ -106,7 +128,11 @@ public class MapaFragment extends BaseFragment implements OnMapReadyCallback {
                 map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
             }
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onMyLocationChange(Location location) {
+
     }
 }
